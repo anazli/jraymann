7fffa693263b8b6be5b8b7bffd6226c89f8621c4
Andreas Nazlidis <andreas221b@gmail.com>
2024-11-03 21:13:44 +0100

    Initial project structure

---
+++ src/main/java/com/org/raymann/Raymann.java
@@ -0,0 +1,10 @@
+package com.org.raymann;
+
+/**
+ * Hello world!
+ */
+public class Raymann {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/org/raymann/RaymannTest.java
@@ -0,0 +1,19 @@
+package com.org.raymann;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RaymannTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
