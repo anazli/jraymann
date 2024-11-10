8c80fab2e69e44bebf8eea68aeeb593cf4a9a1f4
Andreas Nazlidis <andreas221b@gmail.com>
2024-11-10 14:32:08 +0100

    add vec4 and tests

---
+++ src/main/java/com/org/raymann/mathtools/Vec4.java
@@ -0,0 +1,144 @@
+package com.org.raymann.mathtools;
+
+public class Vec4 {
+    private Float x;
+    private Float y;
+    private Float z;
+    private Float w;
+
+    public Vec4() {
+        this(0.f, 0.f, 0.f, 0.f);
+    }
+
+    public Vec4(Float x, Float y, Float z, Float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public static Vec4 create() {
+        return new Vec4();
+    }
+
+    public static Vec4 create(Float x, Float y, Float z, Float w) {
+        return new Vec4(x, y, z, w);
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public void setZ(Float z) {
+        this.z = z;
+    }
+
+    public void setW(Float w) {
+        this.w = w;
+    }
+
+    public void setAll(Float f) {
+        x = y = z = w = f;
+    }
+
+    public Float x() {
+        return x;
+    }
+
+    public Float y() {
+        return y;
+    }
+
+    public Float z() {
+        return z;
+    }
+
+    public Float w() {
+        return w;
+    }
+
+    public void set(int index, Float value) {
+        if(index > 3 || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        switch (index) {
+            case 0: x = value;break;
+            case 1: y = value;break;
+            case 2: z = value;break;
+            case 3: w = value;break;
+        }
+    }
+
+    public Float get(int index) {
+        if(index < 0 || index > 3) {
+            throw new IndexOutOfBoundsException();
+        }
+        if(index == 0) {
+            return x;
+        }
+        else if(index == 1) {
+            return y;
+        }
+        else if(index == 2) {
+            return z;
+        }
+        return w;
+    }
+
+    public Float length() {
+        double d = Math.sqrt((x * x) + (y * y) + (z * z) + (w * w));
+        return (float) d;
+    }
+
+    public Vec4 normalVector() {
+        Float l = length();
+        return new Vec4(x / l, y / l, z / l, w / l);
+    }
+
+    public Vec4 plus(Vec4 v) {
+        return new Vec4(x + v.x(), y + v.y(), z + v.z(), w + v.w());
+    }
+
+    public Vec4 minus(Vec4 v) {
+        return new Vec4(x - v.x(), y - v.y(), z - v.z(), w - v.w());
+    }
+
+    public Vec4 times(Vec4 v) {
+        return new Vec4(x * v.x(), y * v.y(), z * v.z(), w * v.w());
+    }
+
+    public Vec4 times(Float f) {
+        return new Vec4(x * f, y * f, z * f, w * f);
+    }
+
+    public Vec4 over(Vec4 v) {
+        return new Vec4(x / v.x(), y / v.y(), z / v.z(), w / v.w());
+    }
+
+    public Vec4 over(Float f) {
+        return new Vec4(x / f, y / f, z / f, w / f);
+    }
+
+    public Float dot(Vec4 v) {
+        return x * v.x() + y * v.y() + z * v.z() + w * v.w();
+    }
+
+    public boolean equals(Vec4 v) {
+        return (Math.abs(x - v.x()) < Constants.EPSILON) &&
+                (Math.abs(y - v.y()) < Constants.EPSILON) &&
+                (Math.abs(z - v.z()) < Constants.EPSILON) &&
+                (Math.abs(w - v.w()) < Constants.EPSILON);
+    }
+
+    public Vec4 opposite() {
+        return new Vec4(-x, -y, -z, -w);
+    }
+
+    public String getInfoStr() {
+        return "(" + x + "," + y + "," + z + "," + w + ")";
+    }
+}
---
+++ src/test/java/com/org/raymann/mathtools/Vec4Test.java
@@ -0,0 +1,164 @@
+package com.org.raymann.mathtools;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vec4Test {
+    private Vec4 v;
+
+    @BeforeEach
+    void setUp() {
+        v = Vec4.create();
+    }
+
+    @Test
+    public void givenNullVectorWhenItIsCreatedThenItIsZeroInit() {
+        float f = 0.f;
+        assertEquals(f, v.x());
+        assertEquals(f, v.y());
+        assertEquals(f, v.z());
+        assertEquals(f, v.w());
+    }
+
+    @Test
+    public void givenFloatWhenSetsCoordinateThenItIsCorrectlyUpdated() {
+        float x = -0.5f;
+        float y = 14.4355f;
+        float z = -3.14159f;
+        float w = 99.9f;
+        v.setX(x);
+        v.setY(y);
+        v.setZ(z);
+        v.setW(w);
+        assertEquals(x, v.x());
+        assertEquals(y, v.y());
+        assertEquals(z, v.z());
+        assertEquals(w, v.w());
+    }
+
+    @Test
+    public void givenFloatWhenSetsAllCoordsThenTheyAreCorrectlyUpdated() {
+        float f = 3.14159f;
+        v.setAll(f);
+        assertEquals(f, v.x());
+        assertEquals(f, v.y());
+        assertEquals(f, v.z());
+        assertEquals(f, v.w());
+    }
+
+    @Test
+    public void givenNonNullVectorWhenLengthIsReturnedThenItIsCorrect() {
+        assertEquals((float)Math.sqrt(0.f), v.length());
+        v.setAll(-1.f);
+        assertEquals((float)Math.sqrt(4.f), v.length());
+        v.setAll(3.f);
+        assertEquals((float)Math.sqrt(36.f), v.length());
+    }
+
+    @Test
+    public void givenNonNullVectorWhenItIsNormalizedThenLengthIsCorrect() {
+        v.setX(4.53f);
+        v.setY(93.5f);
+        v.setZ(-56.3f);
+        v.setW(3.14f);
+        v = v.normalVector();
+        assertEquals(1.f, v.length());
+    }
+
+    @Test
+    public void givenTwoVectorsWhenAddedThenResultedVectorIsCorrect() {
+        v.setAll(5.2f);
+        Vec4 u = Vec4.create(3.14159f, -45.9f, 200.5346f, 0.34f);
+        Vec4 w = v.plus(u);
+        assertEquals(8.34159f, w.x());
+        assertEquals(-40.7f, w.y());
+        assertEquals(205.7346f, w.z());
+        assertEquals(5.54f, w.w());
+    }
+
+    @Test
+    public void givenTwoVectorsWhenSubtractedThenResultedVectorIsCorrect() {
+        v.setAll(10.f);
+        Vec4 u = Vec4.create(10.f, 10.f, 10.f, 10.f);
+        Vec4 w = v.minus(u);
+        assertEquals(0.f, w.x());
+        assertEquals(0.f, w.y());
+        assertEquals(0.f, w.z());
+        assertEquals(0.f, w.w());
+    }
+
+    @Test
+    public void givenTwoVectorsWhenMultipliedThenResultedVectorIsCorrect() {
+        v.setAll(9.325f);
+        Vec4 u = Vec4.create(9.2f, -15.f, 7.5f, 1.f);
+        Vec4 w = v.times(u);
+        assertEquals(85.78999f, w.x());
+        assertEquals(-139.875f, w.y());
+        assertEquals(69.9375f, w.z());
+        assertEquals(9.325f, w.w());
+    }
+
+    @Test
+    public void givenVectorAndFloatWhenMultipliedThenResultedVectorIsCorrect() {
+        Float f = 9.325f;
+        v = Vec4.create(9.2f, -15.f, 7.5f, 0.f);
+        Vec4 w = v.times(f);
+        assertEquals(85.78999f, w.x());
+        assertEquals(-139.875f, w.y());
+        assertEquals(69.9375f, w.z());
+        assertEquals(0.f, w.w());
+    }
+
+    @Test
+    public void givenTwoVectorsWhenDividedThenResultedVectorIsCorrect() {
+        v.setAll(9.325f);
+        Vec4 u = Vec4.create(9.2f, -15.f, 7.5f, 1.f);
+        Vec4 w = v.over(u);
+        assertEquals(1.013587f, w.x());
+        assertEquals(-0.62166667f, w.y());
+        assertEquals(1.2433333f, w.z());
+        assertEquals(9.325f, w.w());
+    }
+
+    @Test
+    public void givenVectorAndFloatWhenDividedThenResultedVectorIsCorrect() {
+        Float f = 9.325f;
+        v = Vec4.create(9.2f, -15.f, 7.5f, 0.f);
+        Vec4 w = v.over(f);
+        assertEquals(0.98659515f, w.x());
+        assertEquals(-1.6085792f, w.y());
+        assertEquals(0.8042896f, w.z());
+        assertEquals(0.f, w.w());
+    }
+
+    @Test
+    public void givenTwoVectorsWhenDotIsComputedThenItIsCorrect() {
+        v.setAll(3.14159f);
+        Vec4 u = Vec4.create(9.2f, -15.f, 7.5f, 0.f);
+        assertEquals(5.340702f, v.dot(u));
+    }
+
+    @Test
+    public void givenTwoEqualVectorsWhenTheyAreComparedThenReturnsTrue() {
+        v.setX(1.f);
+        v.setY(2.f);
+        v.setZ(3.f);
+        v.setW(0.f);
+        Vec4 u = Vec4.create(1.f, 2.f, 3.f, 0.f);
+        assertTrue(v.equals(u));
+        v.setAll(5.f);
+        assertFalse(v.equals(u));
+    }
+
+    @Test
+    public void givenNonNullVectorWhenNegatedThenReturnsOpposite() {
+        v.setAll(43.87f);
+        Vec4 u = v.opposite();
+        assertEquals(-v.x(), u.x());
+        assertEquals(-v.y(), u.y());
+        assertEquals(-v.z(), u.z());
+        assertEquals(-v.w(), u.w());
+    }
+}
