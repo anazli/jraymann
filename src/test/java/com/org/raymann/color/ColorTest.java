435a8857930e6044d9e762db8e96f76d742fbda3
Andreas Nazlidis <andreas221b@gmail.com>
2024-11-10 12:57:50 +0100

    refactor color tests

--- src/test/java/com/org/raymann/color/ColorTest.java
+++ src/test/java/com/org/raymann/color/ColorTest.java
@@ -13,27 +13,27 @@ class ColorTest {
         Color c1 = Color.create(0.9f, 0.6f, 0.75f);
         Color c2 = Color.create(0.7f, 0.1f, 0.25f);
         Color c3 = c1.plus(c2);
-        assertTrue(Math.abs(c3.red() - 1.6f) < eps);
-        assertTrue(Math.abs(c3.green() - 0.7f) < eps);
-        assertTrue(Math.abs(c3.blue() - 1.0f) < eps);
+        assertEquals(1.6f, c3.red(), eps);
+        assertEquals(0.7f, c3.green(), eps);
+        assertEquals(1.f, c3.blue(), eps);
     }
     @Test
     public void givenTwoColorsWhenSubtractedThenCorrectColorIsReturned() {
         Color c1 = Color.create(0.9f, 0.6f, 0.75f);
         Color c2 = Color.create(0.7f, 0.1f, 0.25f);
         Color c3 = c1.minus(c2);
-        assertTrue(Math.abs(c3.red() - 0.2f) < eps);
-        assertTrue(Math.abs(c3.green() - 0.5f) < eps);
-        assertTrue(Math.abs(c3.blue() - 0.5f) < eps);
+        assertEquals(0.2f, c3.red(), eps);
+        assertEquals(0.5f, c3.green(), eps);
+        assertEquals(0.5f, c3.blue(), eps);
     }
 
     @Test
     public void givenColorAndFloatWhenMultipliedThenCorrectColorIsReturned() {
         Color c = Color.create(0.2f, 0.3f, 0.4f);
         c = c.times(2.f);
-        assertTrue(Math.abs(c.red() - 0.4f) < eps);
-        assertTrue(Math.abs(c.green() - 0.6f) < eps);
-        assertTrue(Math.abs(c.blue() - 0.8f) < eps);
+        assertEquals(0.4f, c.red(), eps);
+        assertEquals(0.6f, c.green(), eps);
+        assertEquals(0.8f, c.blue(), eps);
     }
 
     @Test
@@ -41,9 +41,9 @@ class ColorTest {
         Color c1 = Color.create(1.f, 0.2f, 0.4f);
         Color c2 = Color.create(0.9f, 1.f, 0.1f);
         Color c3 = c1.times(c2);
-        assertTrue(Math.abs(c3.red() - 0.9f) < eps);
-        assertTrue(Math.abs(c3.green() - 0.2f) < eps);
-        assertTrue(Math.abs(c3.blue() - 0.04f) < eps);
+        assertEquals(0.9f, c3.red(), eps);
+        assertEquals(0.2f, c3.green(), eps);
+        assertEquals(0.04f, c3.blue(), eps);
     }
 
 }
